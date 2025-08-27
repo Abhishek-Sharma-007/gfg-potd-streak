ba8071209b3d3df691f2d641860e5fcc3354bb4a
Abhishek-Sharma-007 <abhisheksharma14077@gmail.com>
2025-08-28 02:32:07 +0530

    ci(java): compile each problem in its own folder to avoid class name clashes; keep Python checks soft

--- problems/2025/08-27/count-possible-triangles/Solution.java
+++ problems/2025/08-27/count-possible-triangles/Solution.java
@@ -1,26 +1,34 @@
-import java.util.*;
+package problems.y2025.d08_27.count_possible_triangles;
+
+import java.util.Arrays;
+
 public class Solution {
     public static long countTriangles(int[] arr) {
         int n = arr.length;
         if (n < 3) return 0;
+
         Arrays.sort(arr);
         long count = 0;
-        for (int k = n-1; k >= 2; k--) {
-            int i = 0, j = k-1;
+
+        // Fix k and use two pointers i,j < k
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0, j = k - 1;
             while (i < j) {
                 if (arr[i] + arr[j] > arr[k]) {
+                    // All pairs (i..j-1, j) work
                     count += (j - i);
                     j--;
-                } else i++;
+                } else {
+                    i++;
+                }
             }
         }
         return count;
     }
+
     public static void main(String[] args) {
-        System.out.println(countTriangles(new int[]{4,6,3,7})); // 3
-        System.out.println(countTriangles(new int[]{10,21,22,100,101,200,300})); // 6
-        System.out.println(countTriangles(new int[]{1,2,3})); // 0
+        System.out.println(countTriangles(new int[]{4, 6, 3, 7}));                 // 3
+        System.out.println(countTriangles(new int[]{10,21,22,100,101,200,300}));   // 6
+        System.out.println(countTriangles(new int[]{1, 2, 3}));                    // 0
     }
 }
-
-
